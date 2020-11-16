3bc3ea0101f9eafc99d418cd8277a41d2ef70299
Kieran Uddin <kieranbehn@gmail.com>
2020-11-16 20:09:04 +0000

    Correct player join/quit grammar mistake

--- src/main/java/minedisco/minecraft/PlayerJoinQuitListener.java
+++ src/main/java/minedisco/minecraft/PlayerJoinQuitListener.java
@@ -21,13 +21,13 @@ public class PlayerJoinQuitListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void playerJoin(PlayerJoinEvent event) {
-        bot.sendMessageToChannel(event.getPlayer().getName() + " logged in. There is now "
+        bot.sendMessageToChannel(event.getPlayer().getName() + " logged in. There are now "
                 + this.server.getOnlinePlayers().size() + " players online.");
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void playerQuit(PlayerQuitEvent event) {
-        bot.sendMessageToChannel(event.getPlayer().getName() + " disconnected. There is now "
+        bot.sendMessageToChannel(event.getPlayer().getName() + " disconnected. There are now "
                 + (this.server.getOnlinePlayers().size() - 1) + " players online.");
     }
 
